a2cc7ad6f1395fdf6190c73fd65441a249beea5a
Vicente clase <vayusop01@educarex.es>
2026-09-17 20:39:11 +0200

    Web de adivinar el numero secreto

---
+++ DWEC/ut2-javascript/ejemplos/adivinanumero/adivinanumero.js
@@ -0,0 +1,28 @@
+//ADIVINA EL NÚMERO
+
+const INTENTOS_MAXIMOS = 5;  // Número máximo de intentos permitidos
+const NUMERO_MAXIMO = 10; // Número máximo que el usuario puede adivinar (0 a NUMERO_MAXIMO)
+
+let numeroSecreto = Math.floor(Math.random() * NUMERO_MAXIMO); // Genera un número aleatorio entre 0 y NUMERO_MAXIMO
+let intentos = 0; // Contador de intentos realizados
+let acertado = false; 
+
+
+while (intentos < INTENTOS_MAXIMOS && !acertado) { // Bucle que se ejecuta mientras el usuario no haya adivinado el número y no haya superado el número máximo de intentos
+    let numeroUsuario = Number(prompt(`Adivina el número (entre 0 y ${NUMERO_MAXIMO}). Intento ${intentos + 1} de ${INTENTOS_MAXIMOS}:`));
+    intentos++;
+
+    if (numeroUsuario === numeroSecreto) { //Si el numero ingresado por el usuario es igual al secreto, lanza una alerta de correcto
+        alert(`¡Felicidades! Has adivinado el número secreto ${numeroSecreto} en ${intentos} intentos.`);
+        acertado = true;
+    }
+
+    if (numeroUsuario < numeroSecreto) { //Si el numero ingresado por el usuario es menor al secreto, lanza una alerta de mayor
+        alert("El número secreto es mayor. Intenta de nuevo.");
+    } else if (numeroUsuario > numeroSecreto) { //Si el numero ingresado por el usuario es mayor al secreto, lanza una alerta de menor
+        alert("El número secreto es menor. Intenta de nuevo.");
+    } 
+    else { // Si el número ingresado no es válido, lanza una alerta indicando que se debe introducir un número válido
+        alert("Por favor, introduce un número válido.");
+    }
+}
